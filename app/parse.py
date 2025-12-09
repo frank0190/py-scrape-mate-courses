b080d7e52563313c9b0ad5e12b362b99819eeb2e
Vadym Piddubnyi <leegrad@ukr.net>
2025-12-08 23:12:52 -0600

    Solution

--- app/parse.py
+++ app/parse.py
@@ -1,8 +1,16 @@
+from dataclasses import dataclass
 import requests
 from bs4 import BeautifulSoup, Tag
-from dataclasses import dataclass
+import csv
 
-URL_TO_SCRAPING = "https://mate.academy"
+
+URL = "https://mate.academy"
+
+
+def get_html(url: str) -> str:
+    response = requests.get(url)
+    response.raise_for_status()
+    return response.text
 
 
 @dataclass
@@ -10,33 +18,48 @@ class Course:
     name: str
     short_description: str
     duration: str
+    modules_count: int = 0
 
 
-def parse_courses(blocks: list[Tag]) -> list[Course]:
-    courses = []
-
-    for block in blocks:
-        paragraphs = block.select("p")
-        duration = paragraphs[0].get_text(strip=True)
-        short_description = paragraphs[1].get_text(strip=True)
-        name = block.select_one("h3").get_text(strip=True)
-
-        courses.append(Course(
-            name=name,
-            short_description=short_description,
-            duration=duration
-        ))
-
-    return courses
+def parse_courses(course_block: Tag) -> Course:
+    return Course(
+        name=course_block.select_one(
+            "h3.ProfessionCard_title__m7uno").text.strip(),
+        short_description=course_block.select_one(
+            "p.ProfessionCard_description__K8weo").text.strip(),
+        duration=course_block.select_one(
+            "p.ProfessionCard_duration__13PwX").text.strip(),
+    )
 
 
 def get_all_courses() -> list[Course]:
-    response = requests.get(URL_TO_SCRAPING)
+    courses = []
+    url = URL + "/courses"
+    response = requests.get(url)
     soup = BeautifulSoup(response.content, "html.parser")
+    for course_block in soup.select(
+            "div.ProfessionCard_content__mPiVi"):
+        courses.append(parse_courses(course_block))
+    return courses
 
-    blocks = soup.select(".ProfessionCard_content__mPiVi")
-    return parse_courses(blocks)
+
+def save_courses_to_csv(
+        courses: list[Course], output_csv_path: str
+) -> None:
+    with open(output_csv_path, "w", newline="", encoding="utf-8") as f:
+        writer = csv.writer(f)
+        writer.writerow(["name", "short_description", "duration"])
+        for course in courses:
+            writer.writerow([
+                course.name,
+                course.short_description,
+                course.duration])
+
+
+def main(output_csv_path: str) -> None:
+    courses = get_all_courses()
+    save_courses_to_csv(courses, output_csv_path)
 
 
 if __name__ == "__main__":
-    print(get_all_courses())
+    main("courses.csv")
